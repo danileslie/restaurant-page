b7ad104df120b493d79452fd2dd8deb1c79688f9
Dani <s.leslie94@gmail.com>
2022-10-24 11:36:31 -0400

    edit home page and footer

--- src/modules/home.js
+++ src/modules/home.js
@@ -1,20 +1,23 @@
 const loadHome = () => {
     const content = document.querySelector('#content');
+    const homeTab = document.querySelector('li');
     const test = document.createElement('div');
     test.setAttribute('id', 'home');
     test.setAttribute('data-content', '');
     test.classList.add('tabContents');
     test.classList.add('active');
+    homeTab.classList.add('active');
+   
     test.innerHTML = `
     <div class='home-content'>
     <div class='left'>
-    <div class ='phrase'>
-        <h1>Best Food In All Of Eorzea!</h1>
-    </div>
     <img class='cafe-pic' src= './images/Cafe-Eorzea-Decor.png' alt='cafe with seating'/>
     </div>
     <div class='right'>
     <div class="blurb">
+    <div class ='phrase'>
+    <h1>Best Food In All Of Eorzea!</h1>
+</div>
         <p>A traveling restaurant dedicated to bringing you the most delicious food in all of Eorzea, The Busy Behemoth is committed to making sure that no meal here will be your last!</p> 
         <p>The quality of our fresh foods and electrifying flavour will ensure that not even an ecliptic meteor will stop you from eating your fill.</p>
     </div>
