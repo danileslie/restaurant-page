bde4af2c5bf987a956bab9e5e4d0a6eb91a9e766
Dani <s.leslie94@gmail.com>
2022-10-27 10:24:14 -0400

    update README

--- src/modules/footer.js
+++ src/modules/footer.js
@@ -6,7 +6,7 @@ footer.classList.add('footer');
 footer.innerHTML = `
 <p>Copyright © 2021 danileslie</p>
 <a href='https://github.com/danileslie'>
-<img class='footer-icon' src=./images/GitHub-Mark-32px.png></a>
+<img class='footer-icon' src=./images/GitHub-Mark-32px.png></a> 
 `;
 content.appendChild(footer);
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,13 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 
 module.exports = {
     mode: 'production',
   entry: './src/index.js',
-  plugins: [
-    new HtmlWebpackPlugin({
-        title: 'The Busy Behemoth',
-        filename: 'index.html',
-    }),
-  ],
+
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist/'),
   },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.html$/i,
-        use: 'html-loader'
-    },
-    {
-        test: /\.(png|jpg)$/i,
-        type: 'asset/resource',
-        generator: {
-            filename: 'images/[name][ext]'
-        }
-    }
-    ],
-  },
+
 };
